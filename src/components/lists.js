703ecc1a35f40d8951985b764ac2ce3006b83132
Blayne Marjama <blayne.marjama@gmail.com>
2017-03-26 19:48:18 -0700

    figured out the function, implemented it into lists for the specific url

--- src/components/lists.js
+++ src/components/lists.js
@@ -5,17 +5,32 @@ import { Link } from 'react-router-dom';
 import { getUrlParams } from '../functions/get_url';
 
 class Lists extends Component {
+
+	// these are basically the exact same code, aside from the ending having a county code or not
+	// should probably refactor it...
+	// okay, im refactoring it.
 	renderLists() {
 		return this.props.lists.map((list) => {
-			getUrlParams(list.url)
-			return (
-				<Link to={"/lists/" + list.pid} key={list.pid} >
-					<li className="list-group-item">
-						<span className="pull-xs-right">{list.date}</span>
-						<strong>{list.title}</strong>
-					</li>
-				</Link>
-			)
+			const urlParams = getUrlParams(list.url);
+			if (urlParams[1] === null) {
+				return (
+					<Link to={`/lists/${urlParams[0]}/${urlParams[2]}/${urlParams[3]}`} key={list.pid} >
+						<li className="list-group-item">
+							<span className="pull-xs-right">{list.date}</span>
+							<strong>{list.title}</strong>
+						</li>
+					</Link>
+				)
+			} else {
+				return (
+					<Link to={`/lists/${urlParams[0]}/${urlParams[2]}/${urlParams[3]}/${urlParams[1]}`} key={list.pid} >
+						<li className="list-group-item">
+							<span className="pull-xs-right">{list.date}</span>
+							<strong>{list.title}</strong>
+						</li>
+					</Link>
+				)
+			}
 		})
 	}
 
--- src/functions/get_url.js
+++ src/functions/get_url.js
@@ -1,3 +1,22 @@
 export function getUrlParams(url) {
-	console.log(url)
+	function urlSplit(toSplit, charTo){
+ 	 return toSplit.split(charTo);
+	}
+
+  function hasCounty(urlparams) {
+    if (/\d/.test(urlparams[4]) === true){
+      var city = urlSplit(urlparams[2], '.')[0];
+      var county = null;
+      var categoryId = urlparams[3];
+      var pid = urlSplit(urlparams[4], '.')[0];
+      return [city, county, categoryId, pid];
+    } else {
+      city = urlSplit(urlparams[2], '.')[0];
+      county = urlparams[3];
+      categoryId = urlparams[4];
+      pid = urlSplit(urlparams[5], '.')[0];
+      return [city, county, categoryId, pid];
+    }
+  }
+return (hasCounty(urlSplit(url, '/')));
 }
