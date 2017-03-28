21d72025804db6a6d49b5c48409e3c2a40cdf3e3
Blayne Marjama <blayne.marjama@gmail.com>
2017-03-27 23:49:55 -0700

    added labels, and statesgit status

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -9,8 +9,9 @@ class SearchBar extends Component {
 
 		// change citySelect/categorySelect value based on whatever the first selecter is
 		// otherwise if the user does not change their selection, the value will be undefined
-		this.state = { term: '', citySelect: 'Miami', categorySelect: 'ggg'};
+		this.state = { term: '', stateSelect:'Florida', citySelect: 'Miami', categorySelect: 'ggg'};
 		this.onInputChange = this.onInputChange.bind(this);
+		this.onStateSelect = this.onStateSelect.bind(this);
 		this.onCitySelect = this.onCitySelect.bind(this);
 		this.onCategorySelect = this.onCategorySelect.bind(this);
 		this.onSearchSubmit = this.onSearchSubmit.bind(this);
@@ -27,6 +28,11 @@ class SearchBar extends Component {
 	}
 
 	// this sets the state of the city selector
+	onStateSelect(event) {
+		this.setState({ stateSelect: event.target.value });
+	}
+
+	// this sets the state of the city selector
 	onCitySelect(event) {
 		this.setState({ citySelect: event.target.value });
 	}
@@ -37,9 +43,9 @@ class SearchBar extends Component {
 	onSearchSubmit(event) {
 		event.preventDefault();
 
-		const { citySelect, categorySelect, term } = this.state;
-		// console.log(this.state.term, this.state.citySelect, this.state.categorySelect);
-		this.props.fetchLists(citySelect, categorySelect, term);
+		const { stateSelect, citySelect, categorySelect, term } = this.state;
+		// this.props.fetchLists(citySelect, categorySelect, term);
+		console.log(stateSelect, citySelect, categorySelect, term);
 		// this sets the search bar back to blank, to show the user they actually searched for something
 		this.setState({ term: '' });
 	}
@@ -48,23 +54,41 @@ class SearchBar extends Component {
 	render() {
 		return (
 			<form onSubmit={this.onSearchSubmit} className="input-group">
-				<select 
-					className="form-control"
-					value={this.state.citySelect}
-					onChange={this.onCitySelect} >
-					<option value="Miami">Miami</option>
-					<option value="Denver">Denver</option>
-					<option value="Portland">Portland</option>
-				</select>
+				<div className="form-group">
+					<label style={{backgroundColor:"red"}}>States</label>
+					<select 
+						className="form-control"
+						value={this.state.stateSelect}
+						onChange={this.onStateSelect} >
+						<option value="Florida">Florida</option>
+						<option value="Colorado">Colorado</option>
+						<option value="Oregon">Oregon</option>
+					</select>
+				</div>
 
-				<select
-					className="form-control"
-					value={this.state.categorySelect}
-					onChange={this.onCategorySelect} >
-					<option value="ggg">gigs</option>
-					<option value="sss">for sale</option>
-					<option value="jjj">jobs</option>
-				</select>
+				<div className="form-group">
+					<label style={{backgroundColor:"red"}}>City</label>
+					<select 
+						className="form-control"
+						value={this.state.citySelect}
+						onChange={this.onCitySelect} >
+						<option value="Miami">Miami</option>
+						<option value="Denver">Denver</option>
+						<option value="Portland">Portland</option>
+					</select>
+				</div>
+
+				<div className="form-group">
+					<label style={{backgroundColor:"red"}}>Category Select</label>
+					<select
+						className="form-control"
+						value={this.state.categorySelect}
+						onChange={this.onCategorySelect} >
+						<option value="ggg">gigs</option>
+						<option value="sss">for sale</option>
+						<option value="jjj">jobs</option>
+					</select>
+				</div>
 
 				<input 
 					placeholder="search term"
