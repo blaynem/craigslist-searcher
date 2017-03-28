cad026a80cd376f711fad2e8196a3713b966db6b
Blayne Marjama <blayne.marjama@gmail.com>
2017-03-27 21:49:18 -0700

    here's a bunch of categories, don't think the others will be that useful for me realistically

---
+++ src/data/categories.js
@@ -0,0 +1,97 @@
+const categories = [
+	{
+    category: "housing",
+    catCode: "hhh",
+    codes: {
+      "apts & housing": "apa",
+			"housing swap": "swp",
+			"housing wanted": "hsw",
+			"office & commercial": "off",
+			"parking & storage": "prk",
+			"real estate for sale": "rea",
+			"rooms & shared": "roo",
+			"rooms wanted": "sha",
+			"sublets & temporary": "sub",
+			"vacation rentals": "vac"
+  	}
+  },
+  {
+	  category: "gigs",
+	  catCode: "ggg",
+	  codes: {
+      computer: "cpg",
+			creative: "crg",
+			crew: "cwg",
+			domestic: "dmg",
+			event: "evg",
+			labor: "lbg",
+			talent: "tlg",
+			writing: "wrg"
+	  }
+	},
+	{
+	  category: "jobs",
+	  catCode: "jjj",
+	  codes: {
+      "accounting & finance": "acc",
+			"admin & office": "ofc",
+			"arch & engineering": "egr",
+			"art & media & design": "med",
+			"biotech & science": "sci",
+			"business & mgmt": "bus",
+			"customer service": "csr",
+			"education": "edu",
+			"food & bev & hosp": "fbh",
+			"general labor": "lab",
+			"government": "gov",
+			"human resources": "hum",
+			"internet engineers": "eng",
+			"legal & paralegal": "lgl",
+			"manufacturing": "mnu",
+			"marketing & pr & ads": "mar",
+			"medical & health": "hea",
+			"nonprofit sector": "npo",
+			"real estate": "rej",
+			"retail & wholesale": "ret",
+			"sales & biz dev": "sls",
+			"salon & spa & fitness": "spa",
+			"security": "sec",
+			"skilled trade & craft": "trd",
+			"software & qa & dba": "sof",
+			"systems & network": "sad",
+			"technical support": "tch",
+			transport: "trp",
+			"tv & film & video": "tfr",
+			"web & info design": "web",
+			"writing & editing": "wri",
+			"ETC": "etc"
+	  }
+	},
+  {
+	  category: "services",
+	  catCode: "bbb",
+	  codes: {
+			"automotive": "aos",
+			"beauty": "bts",
+			"cell & mobile": "cms",
+			"computer": "cps",
+			"creative": "crs",
+			"cycle": "cys",
+			"event": "evs",
+			"farm & garden": "fgs",
+			"financial": "fns",
+			"household": "hss",
+			"labor moving": "lbs",
+			"legal": "lgs",
+			"lessons": "lss",
+			"marine": "mas",
+			"pet": "pas",
+			"real estate": "rts",
+			"skilled trade": "sks",
+			"small biz ads": "biz",
+			"therapeutic": "thp",
+			"travel vacactions": "trv",
+			"write editing translation": "wet"
+	  }
+	}
+]
